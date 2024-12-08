588730c5f4fbedfa50dd88779cc0081775809861
AhseneRemmouche <ahsene.remmouche@outlook.com>
2024-12-08 17:39:26 -0500

    add home structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import Contact from "./components/contact/Contact";
+import Hero from "./components/hero/Hero";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+
 function App() {
 	return (
-		<>
-			<h1>Bismi Allah</h1>
-		</>
+		<div className="container">
+			{/* <h1>Bismi Allah</h1> */}
+			<section id="hero">
+				<Hero />
+			</section>
+			<section id="services">
+				<Services />
+			</section>
+			<section id="portfolio">
+				<Portfolio />
+			</section>
+			<section id="contact">
+				<Contact />
+			</section>
+		</div>
 	);
 }
 
