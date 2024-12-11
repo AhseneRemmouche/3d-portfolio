84bbe1f4510d589acf0a772f13e4af2806238c66
AhseneRemmouche <ahsene.remmouche@outlook.com>
2024-12-11 07:49:58 -0500

    add text speech animation css

--- src/components/hero/speech/Speech.jsx
+++ src/components/hero/speech/Speech.jsx
@@ -1,10 +1,28 @@
-const Speech = ()=>{
-    return (
-			<div className="bubbleContainer">
-				<div className="bubble">Speech comments</div>
-				<img src="images/man.png" alt="" />
+import { TypeAnimation } from "react-type-animation";
+
+const Speech = () => {
+	return (
+		<div className="bubbleContainer">
+			<div className="bubble">
+				<TypeAnimation
+					sequence={[
+						1000,
+						"Hi! I'm a Full Stack Developer",
+						1000,
+						"I create beautiful web experiences",
+						1000,
+						"Let's build something amazing together!",
+						1000,
+					]}
+					wrapper="span"
+					speed={50}
+					deletionSpeed={40}
+					repeat={Infinity}
+				/>
 			</div>
-		);
-}
+			<img src="images/man.png" alt="" />
+		</div>
+	);
+};
 
 export default Speech;
