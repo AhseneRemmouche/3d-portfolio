c40ad099a82d669314139a87db074b66ba87e1d4
AhseneRemmouche <ahsene.remmouche@outlook.com>
2024-12-26 07:55:58 -0500

    add test for text animation

--- src/components/TextAnimation.jsx
+++ src/components/TextAnimation.jsx
@@ -1,7 +1,33 @@
 import { easeInOut } from "motion";
 import { motion } from "motion/react";
+import { transition } from "three/examples/jsm/tsl/display/TransitionNode.js";
 
 const TextAnimation = () => {
+	const shapeVariant = {
+		initialCircle: {
+			x: 200,
+			y: 200,
+			opacity: 0,
+		},
+		animateCircle: {
+			x: 0,
+			y: 0,
+			opacity: 0.5,
+			transition: {
+				duration: 3,
+				repeat: Infinity,
+			},
+		},
+		initialSquare: {
+			x: 0,
+			y: 0,
+		},
+		animateSquare: {
+			x: 200,
+			y: 200,
+		},
+	};
+
 	return (
 		<section
 			style={{
@@ -11,20 +37,30 @@ const TextAnimation = () => {
 			}}
 		>
 			<motion.div
-				initial={{ x: 0, y: 0, opacity: 0 }}
-				animate={{ x: 100, y: -100, opacity: 1 }}
-				transition={{
-					duration: 2,
-					delay: 0,
-					ease: easeInOut,
-					repeat: Infinity,
-				}}
 				style={{
 					width: 200,
 					height: 200,
 					borderRadius: 100,
 					backgroundColor: "blueviolet",
 				}}
+				// initial={{ x: 0, y: 0, opacity: 0 }}
+				// animate={{ x: [0, 200], y: [0, -200], opacity: [0, 1] }}
+				// transition={{
+				// 	duration: 2,
+				// 	// delay: 0,
+				// 	ease: easeInOut,
+				// 	repeat: Infinity,
+				// }}
+
+				variants={shapeVariant}
+				initial="initialCircle"
+				animate="animateCircle"
+			></motion.div>
+			<motion.div
+				style={{ width: 200, height: 200, backgroundColor: "orange" }}
+				variants={shapeVariant}
+				initial="initialSquare"
+				animate="animateSquare"
 			></motion.div>
 		</section>
 	);
