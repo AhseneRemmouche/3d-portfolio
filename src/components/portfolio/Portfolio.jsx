f02d2041a14c45dab8d57af67e6d5da2c16f5595
Ahsene Remmouche <ahsene.remmouche@outlook.com>
2025-01-02 22:38:25 +0000

    add portfolio section content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,46 +1,71 @@
 import "./portfolio.css";
 
-const items = [
+const projects = [
 	{
 		id: 1,
-		img: "/p1.jpg",
+		img: "images/p1.jpg",
 		title: "Full Stack Blog Application",
 		desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
 		link: "/",
 	},
 	{
 		id: 2,
-		img: "/p2.jpg",
+		img: "images/p2.jpg",
 		title: "School Management System",
 		desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
 		link: "/",
 	},
 	{
 		id: 3,
-		img: "/p3.jpg",
+		img: "images/p3.jpg",
 		title: "Real-time Chat Application",
 		desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
 		link: "/",
 	},
 	{
 		id: 4,
-		img: "/p4.jpg",
+		img: "images/p4.jpg",
 		title: "Social Media Project",
 		desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
 		link: "/",
 	},
 	{
 		id: 5,
-		img: "/p5.jpg",
+		img: "images/p5.jpg",
 		title: "Animated Portfolio Website",
 		desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
 		link: "/",
 	},
 ];
 
+const Project = ({ item }) => {
+	return (
+		<div>
+			<title>static content</title>
+			<div>
+				<img src={item.img} />
+			</div>
+			<div>
+				<h2>item.title</h2>
+				<p>item.desc</p>
+				<a href={item.link}>
+					<button>View Project</button>
+				</a>
+			</div>
+		</div>
+	);
+};
 
+const ProjectsList = () => {
+	return(projects.map((item) => <Project key={item.id} item={item} />));
+};
 const Portfolio = () => {
-	return <div className="">Portfolio</div>;
+	return (
+		<div className="">
+			<h2>Projects List</h2>
+			<ProjectsList />
+		</div>
+	);
 };
 
 export default Portfolio;
