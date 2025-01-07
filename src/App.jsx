9744b66890d8639079b3572629aa5efcdc0a282f
Ahsene Remmouche <ahsene.remmouche@outlook.com>
2025-01-07 20:07:15 +0000

    add svg animation to contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
 import Animation from "./components/Animation";
 import Animation3d from "./components/Animation3d";
-import Contact from "./components/contact/Contact";
-import Hero from "./components/hero/Hero";
-import Portfolio from "./components/portfolio/Portfolio";
-import Services from "./components/services/Services";
-import TextAnimation from "./components/TextAnimation";
+// import Contact from "./components/contact/Contact";
+// import Hero from "./components/hero/Hero";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Services from "./components/services/Services";
 
+import { lazy, Suspense } from "react";
+import LazyLoad from "react-lazyload";
+
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Services = lazy(() => import("./components/services/Services"));
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+const Contact = lazy(() => import("./components/contact/Contact"));
 function App() {
 	return (
 		<div className="container">
 			{/* <h1>Bismi Allah</h1> */}
-			<section id="hero">
-				<Hero />
-			</section>
-			<section id="services">
-				<Services />
-			</section>
-			{/* <section id="portfolio"> */}
-				<Portfolio />
-			{/* </section> */}
-			<section id="contact">
-				<Contact />
-			</section>
+			<Suspense fallback={"...loading"}>
+				<LazyLoad height={"100vh"} offset={-100}>
+					<section id="home">
+						<Hero />
+					</section>
+				</LazyLoad>
+			</Suspense>
+			<Suspense fallback={"...loading"}>
+				<LazyLoad height={"100vh"} offset={-100}>
+					<section id="services">
+						<Services />
+					</section>
+				</LazyLoad>
+			</Suspense>
+			<Suspense fallback={"loading..."}>
+				<LazyLoad height={"600vh"} offset={-100}>
+					{/* <section id="portfolio"> */}
+					<Portfolio />
+					{/* </section> */}
+				</LazyLoad>
+			</Suspense>
+			<Suspense fallback={"...loading"}>
+				<LazyLoad height={"100vh"} offset={-100}>
+					<section id="contact">
+						<Contact />
+					</section>
+				</LazyLoad>
+			</Suspense>
+
 			{/* <Animation3d /> */}
 			{/* <Animation /> */}
 			{/* <TextAnimation /> */}
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,31 +1,102 @@
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import { motion, useInView } from "motion/react";
 import "./contact.css";
+import ContactSvg from "./ContactSvg";
+
+const listVariant = {
+	initial: {
+		x: 100,
+		opacity: 0,
+	},
+	animate: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			duration: 0.5,
+			staggerChildren: 0.2,
+		},
+	},
+};
 
 const Contact = () => {
+	const form = useRef();
+	const ref = useRef();
+	const isInView = useInView(ref, { margin: "-200px" });
+
+	const [success, setSuccess] = useState(false);
+	const [error, setError] = useState(false);
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				import.meta.env.VITE_SERVICE_ID,
+				import.meta.env.VITE_TEMPLATE_ID,
+				form.current,
+				{
+					publicKey: import.meta.env.VITE_PUBLIC_KEY,
+				}
+			)
+			.then(
+				() => {
+					setSuccess(true);
+					setError(false);
+				},
+				(error) => {
+					setError(true);
+					setSuccess(false);
+				}
+			);
+	};
+
 	return (
-		<div className="contact">
+		<div className="contact" onSubmit={sendEmail} ref={ref}>
 			<div className="contactSection">
-				<form action="">
+				<motion.form
+					variants={listVariant}
+					animate={isInView ? "animate" : "initial"}
+					action=""
+					ref={form}
+				>
 					<h1 className="contactTitle">Let&apos;s Keep in Touch</h1>
-					<div className="formItem">
+					<motion.div className="formItem" variants={listVariant}>
 						<label htmlFor="name">Name</label>
-						<input id="name" type="text" placeholder="John Doe" />
-					</div>
-					<div className="formItem">
+
+						<input
+							id="name"
+							type="text"
+							name="to_name"
+							placeholder="John Doe"
+						/>
+					</motion.div>
+					<motion.div className="formItem" variants={listVariant}>
 						<label htmlFor="email">Email</label>
-						<input id="email" type="text" placeholder="John@gmail.com" />
-					</div>
-					<div className="formItem">
+						<input
+							id="email"
+							type="text"
+							name="from_name"
+							placeholder="John@gmail.com"
+						/>
+					</motion.div>
+					<motion.div className="formItem" variants={listVariant}>
 						<label htmlFor="message">Message</label>
 						<textarea
+							name="message"
 							id="message"
 							rows={10}
 							placeholder="Write your message..."
 						/>
-					</div>
-					<button className="formButton">Send</button>
-				</form>
+					</motion.div>
+					<motion.button className="formButton" variants={listVariant}>
+						Send
+					</motion.button>
+					{success ? <pann>Your message has been sent!</pann> : ""}
+					{error && <span>Something went wrong!</span>}
+				</motion.form>
 			</div>
-			<div className="contactSection">SVG</div>
+			<div className="contactSection"><ContactSvg /></div>
 		</div>
 	);
 };
